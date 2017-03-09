fec9260757ba24c7cb3cd3d5f19e117d3c91d19c
Esteban Jenkins <stbnjenkins@gmail.com>
2017-03-08 20:46:59 -0600

    Improvements in algorithms for managing corner cases

--- algoritmo1.c
+++ algoritmo1.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "point.h"
 
 // function to swap values of points
@@ -16,21 +17,32 @@ void line (Point p0, Point p1){
     printf("Point.x -> %d\nPoint.y -> %d\n", p0.x, p0.y);
     printf("Point.x -> %d\nPoint.y -> %d\n", p1.x, p1.y);
 
+    if(p0.x == p1.x && p0.y == p1.y){
+        printf("(%d, %d)", p0.x, p0.y);
+        return;
+    }
+
     // If p0 is "greater than" p1, swap them
-    if(p0.x > p1.x) swapPoints(&p0, &p1);
+    if(p0.x == p1.x){
+        if(p0.y > p1.y) swapPoints(&p0, &p1);
+    }else{
+        if(p0.x > p1.x) swapPoints(&p0, &p1);
+    }
+    
 
     // Calculate slope and y-intersection
     dx = (p1.x - p0.x);
     dy = (p1.y - p0.y);
 
     m = (long double) dy / dx;
+    //if(m == -INFINITY) m = INFINITY;
     printf("Slope is: %Lf\n\n", m);
 
     b = (long double) p0.y - m * p0.x;
     printf("Y-intersection is: %Lf\n\n", b);
     
     
-    if(dx >= dy){
+    if(abs(dx) >= abs(dy)){
         for(i = p0.x; i <= p1.x; i++){
             y = (long double) m*i + b;
             printf("(%d, %d)", i, (int)round(y));
@@ -41,6 +53,7 @@ void line (Point p0, Point p1){
                 printf("(%d, %d)", p0.x, i);
             }
         }else{
+            if(p0.y > p1.y) swapPoints(&p0, &p1);
             for(i = p0.y; i <= p1.y; i++){
                 y = (long double) (i-b)/m;
                 printf("(%d, %d)", (int)round(y), i);
@@ -51,15 +64,24 @@ void line (Point p0, Point p1){
 }
 
 int main(){
+    // Point p1;
+    // p1.x = 6;
+    // p1.y = 5;
+
+    // Point p2;
+    // p2.x = 8;
+    // p2.y = 17;
+
     Point p1;
     p1.x = 1;
-    p1.y = 2;
+    p1.y = 9;
 
     Point p2;
-    p2.x = 9;
-    p2.y = 4;
+    p2.x = 4;
+    p2.y = 2;
 
-    line(p2,p1);
+    line(p1,p2);
+    // line(p2,p1);
 }
 
 
